573a3b4da96956edc746ca36e5c05892572c3fa3
svetany <svetlana.popach@gmail.com>
2021-09-29 19:29:36 +0300

    3s

--- JavaApplication2/src/Client.java
+++ JavaApplication2/src/Client.java
@@ -1,16 +1,23 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import org.apache.log4j.Logger;
+
+import org.apache.hc.client5.http.impl.sync.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.sync.HttpClients;
+import org.apache.hc.client5.http.methods.CloseableHttpResponse;
+import org.apache.hc.client5.http.methods.HttpPost;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.entity.ContentType;
+import org.apache.hc.core5.http.entity.EntityUtils;
+import org.apache.hc.core5.http.entity.StringEntity;
 
 public class Client {
 static Logger log = Logger.getLogger(Client.class.getName());
-   public static void main(String [] args) throws IOException, InterruptedException {
-    Date timeA = new Date(); // фиксируем время начала кода
-    long startFullCode = timeA.getTime();
+   public static void main(String [] args) throws IOException, InterruptedException, ParseException {
     String site = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?op=NumberToWords";
     HttpURLConnection connection = null;
     URL url;
@@ -19,8 +26,6 @@ static Logger log = Logger.getLogger(Client.class.getName());
     BufferedReader buf = null;
     StringBuilder SB = new StringBuilder();
     int rndNum = (int)(Math.random()*10000);
-    
-
     try{
         //создадим сообщение
         StringBuilder SOAP = new StringBuilder();
@@ -39,11 +44,29 @@ static Logger log = Logger.getLogger(Client.class.getName());
         SOAP.append("</soap:Body>\r\n");
         SOAP.append("</soap:Envelope>");
         
-        byte[] post_message = SOAP.toString().getBytes("UTF-8"); //и превратим его в поток байтов
-        //System.out.println("\r\n"+ "RESPONSE:\r\n" + SOAP.toString()); //выведем, что отослали
-        log.debug("RESPONSE:\r\n" + SOAP.toString()); //выведем, что отослали в log4j
-        
+        //byte[] post_message = SOAP.toString().getBytes("UTF-8"); //и превратим его в поток байтов
+        log.debug("REQUEST:\r\n" + SOAP.toString()); //выведем в log4j, что отослали 
+              
         url = new URL(site); //создаем url, помещаем адрес в url
+        
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(site);
+        httpPost.setHeader("Host", "www.dataaccess.com");
+        httpPost.setHeader("Content-Type", "text/xml; charset=utf-8");
+        //httpPost.setHeader("Content-Length", "length");
+        HttpEntity stringEntity = new StringEntity(SOAP.toString());
+        httpPost.setEntity(stringEntity);
+        Date timeC = new Date();
+        long startRequest = timeC.getTime(); // фиксируем время после отправки
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        Date timeD = new Date(); //// фиксируем время ответа
+        HttpEntity entity = response.getEntity();
+        String resp = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+        long endResponse = timeD.getTime();
+        log.info("\r\n RESPONSE:"+"\r\n ------------------------------------------------\r\n" + resp + "\r\n------------------------------------------------");
+        log.info("Response time, ms: " + (endResponse - startRequest));
+       
+        /*
         connection = (HttpURLConnection)url.openConnection(); //создаем http connection
         connection.setRequestMethod("POST"); //метод отправки
         connection.setDoOutput(true); //позволяет отсылать сообщения
@@ -76,7 +99,7 @@ static Logger log = Logger.getLogger(Client.class.getName());
           Date timeD = new Date(); //// фиксируем время ответа
         long endRequest = timeD.getTime();
            //System.out.println("\r\n"+"Request Time, ms: " + (endRequest - startRequest));
-           log.info("Request Time, ms: " + (endRequest - startRequest));
+           log.info("Time, ms: " + (endRequest - startRequest));
           while((line = buf.readLine()) != null){
               SB.append(line); // добавляем сюда сообщение
              
@@ -87,16 +110,15 @@ static Logger log = Logger.getLogger(Client.class.getName());
        }
       
      //System.out.println("\r\n" + "REQUEST: \r\n" + SB); //выводим полученную после отправки строку, т.е. ответ soap формата    
-     log.debug("REQUEST: " + SB); //выводим полученную после отправки строку, т.е. ответ soap формата в log4j   
-    
+     log.debug("RESPONSE: " + SB); //выводим полученную после отправки строку, т.е. ответ soap формата в log4j   
+    */
     } catch (MalformedURLException e){
      } catch (IOException e) {
-         System.err.println(e.getMessage());
+         log.fatal(e.getMessage());
+
      }     
-     Date timeB = new Date(); // фиксируем время всего кода
-    long endFullCode = timeB.getTime();
-     //System.out.println("\r\n" + "Full Code Time, ms: " + (endFullCode - startFullCode));
-     log.info("Full Code Time, ms: " + (endFullCode - startFullCode));
+  
+    
      
    }
    
